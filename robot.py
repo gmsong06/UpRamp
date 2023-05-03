2d013f5f5f97d408677f0a86631463a8364dab03
Ann Song <gmsong1978@gmail.com>
2023-05-03 10:07:20 -0400

    Working teleop

--- robot.py
+++ robot.py
@@ -1,47 +1,29 @@
 from wpilib import TimedRobot, Joystick, Spark
 import os
 import wpilib
-from drivetrain import Drivetrain
+from robotcontainer import RobotContainer
 
-class MyRobot(TimedRobot) :
+class MyRobot(TimedRobot):
 
     def robotInit(self):
-        '''This method is called as the robot turns on and is often used to setup the
-        joysticks and other presets.'''
-        self.controller = Joystick(0)
-        self.drivetrain= Drivetrain()
-
-
+        self.container = RobotContainer()
 
     def robotPeriodic(self):
-        '''This is called every cycle of the code. In general the code is loop
-        through every .02 seconds.'''
-
-
-    pass
+        pass
 
     def autonomousInit(self):
-        '''This is called once when the robot enters autonomous mode.'''
-
-    1
-    pass
+        pass
 
     def autonomousPeriodic(self):
-        #self.linefollower.run()
-
-        '''This is called every cycle while the robot is in autonomous.'''
+        pass
 
     def teleopInit(self):
         pass
 
     def teleopPeriodic(self):
-        forward = self.controller.getRawAxis(0)
-        rotate = self.controller.getRawAxis(1)
-        self.drivetrain.arcadeDrive(forward, rotate)
-        self.drivetrain.move(forward,rotate)
-        print(forward)
-        print(rotate)
-
+        forward = self.container.controller.getRawAxis(0)
+        rotate = self.container.controller.getRawAxis(1)
+        self.container.drivetrain.arcadeDrive(forward, rotate)
 
 
 if __name__ == "__main__":
